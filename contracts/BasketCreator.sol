d494a9875747d28ee0e219abec1b8fb79ea1b5e1
getjiggy <74795898+getjiggy@users.noreply.github.com>
2021-11-23 15:10:04 -0500

    Update BasketCreator.sol

--- contracts/BasketCreator.sol
+++ contracts/BasketCreator.sol
@@ -9,19 +9,21 @@ import { AddressArrayUtils } from './lib/AddressArrayUtils.sol';
 
 contract BasketCreator {
     using AddressArrayUtils for address[];
-    
+
     event basketCreated(address indexed basketToken, string name, string symbol);
-    
+
     // state variables
-    address LINK = 0x514910771AF9Ca656af840dff83E8264EcF986CA;
+
     constructor() public {
-        
+
     }
     function create(
         address[] memory _components,
         uint256[] memory _units,
         string memory _name,
-        string memory _symbol
+        string memory _symbol,
+        address _vrf_coordinator,
+        address _link
     )
         external
         returns (address)
@@ -29,26 +31,28 @@ contract BasketCreator {
         require(_components.length > 0, "Must have at least 1 component");
         require(_components.length == _units.length, "Component and unit lengths must be the same");
         require(!_components.hasDuplicate(), "Components must not have a duplicate");
-        
+
 
         for (uint256 i = 0; i < _components.length; i++) {
             require(_components[i] != address(0), "Component must not be null address");
             // if a basket were to contain link anyone could drain the link portion of the portfolio by repeatedly calling the lottery function
-            require(_components[i] != address(LINK), "Component can not be Link token, Link is used to secure the lottery");
+            require(_components[i] != address(_link), "Component can not be Link token, Link is used to secure the lottery");
             require(_units[i] > 0, "Units must be greater than 0");
         }
 
-        
+
         BasketToken basketToken = new BasketToken(
             _components,
             _units,
             _name,
-            _symbol
+            _symbol,
+            _vrf_coordinator,
+            _link
             );
-        
-        
+
+
         emit basketCreated(address(basketToken), _name, _symbol);
         return address(basketToken);
     }
-        
+
 }
